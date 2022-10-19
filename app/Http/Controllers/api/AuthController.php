61105e09b2e095e35d0b866bed1b95e22d7747b5
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2022-10-19 22:10:36 +0800

    Issued API Tokens (Auth)

---
+++ app/Http/Controllers/api/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Resources\UserResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        $validated = $request->validated();
+
+        if (Auth::attempt($validated, true)) {
+            $user = Auth::user();
+            $user->access_token =  $user->createToken('authToken')->plainTextToken;
+            return $this->return_api(true, Response::HTTP_OK, null, new UserResource($user), null);
+        } else {
+            return $this->return_api(false, Response::HTTP_UNAUTHORIZED, trans("auth.failed"), null, null);
+        }
+    }
+
+    public function logout()
+    {
+        // Revoke all tokens
+        $user = Auth::user()->tokens()->delete();
+
+        return $this->return_api(true, Response::HTTP_OK, null, null, null);
+    }
+}
